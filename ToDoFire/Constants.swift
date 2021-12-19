84ba5b5976ed3a3d40bad1ec30aed8888020f039
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-12-19 20:22:55 +0200

    add a new ToDoTasks View Controller

---
+++ ToDoFire/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ToDoFire
+//
+//  Created by MacBook on 19.12.2021.
+//
+
+import Foundation
+
+class Constants {
+    static let identifier = "Cell"
+}
---
+++ ToDoFire/ToDoTasksViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ToDoTasksViewController.swift
+//  ToDoFire
+//
+//  Created by MacBook on 19.12.2021.
+//
+
+import UIKit
+
+class ToDoTasksViewController: UIViewController {
+    
+    
+    @IBOutlet weak var toDoTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDoTableView.dataSource = self
+        toDoTableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ToDoTasksViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.identifier, for: indexPath)
+        
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "index path row = \(indexPath.row)"
+        
+        
+        return cell
+    }
+    
+    
+}
