8be0ab0a238a8b91fa32c2b7fd6b68f639200b79
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-12-20 21:53:46 +0200

    add layer.cornerRadius

--- ToDoFire/Controller/InitialViewController.swift
+++ ToDoFire/Controller/InitialViewController.swift
@@ -8,20 +8,24 @@
 import UIKit
 
 class InitialViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loginButton.layer.cornerRadius = 25.0
         // Do any additional setup after loading the view.
     }
 
     @IBAction func loginButtonPressed(_ sender: UIButton) {
         
         // Use the code when you have separate storyboards!!!
+        // Switch from Main to ToDoTasks storyboard
         let vc = UIStoryboard(name: "ToDoTasks", bundle: nil).instantiateViewController(withIdentifier: Constants.storyboardIdentifier)
-        vc.modalPresentationStyle = .fullScreen
         
+        vc.modalPresentationStyle = .fullScreen // open full screen
         present(vc, animated: true, completion: nil)
-        
         
     }
     
