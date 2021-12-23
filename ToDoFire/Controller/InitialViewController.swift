0cfec25ca5318f0822a1e626f2344dcdb4d46cb7
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-12-24 00:50:12 +0200

    add Animation manager

--- ToDoFire/Controller/InitialViewController.swift
+++ ToDoFire/Controller/InitialViewController.swift
@@ -21,10 +21,15 @@ class InitialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loginButton.layer.cornerRadius = 25.0
-        // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 25.0 // add corner radius for login button
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        alarmLabel.alpha = 0 // hide label
+        
     }
-
+    
     @IBAction func loginButtonPressed(_ sender: UIButton) {
         
         // Use the code when you have separate storyboards!!!
@@ -37,6 +42,40 @@ class InitialViewController: UIViewController {
     }
     
     @IBAction func registerButtonPressed(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passwordTextField.text, email != "", password != "" else {
+            
+            warningText("Email or password is incorrect")
+            
+            
+            return }
+        
+        
+    }
+    func warningText(_ text: String) {
+        alarmLabel.text = text
+        
+        // Animation manager
+        UIView.animate(withDuration: 4, delay: 0, options: .autoreverse) { [weak self] in
+            self?.alarmLabel.alpha = 1
+        } completion: { (true) in
+            self.alarmLabel.alpha = 0
+        }
+
+    }
+    
+    
+}
+
+// MARK:- TextField
+
+extension InitialViewController: UITextFieldDelegate {
+    
+    // It happens when return button pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide keyboard
+        emailTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        return true
     }
     
     
